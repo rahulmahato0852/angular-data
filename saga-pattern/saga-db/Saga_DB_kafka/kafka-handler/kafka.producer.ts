dc1c919965ccf11430ced29f51f11342287c7494
rahulmehata0852 <rahulmahato0852@gmail.com>
2024-09-05 11:46:12 +0530

    fixed

--- saga-pattern/saga-db/Saga_DB_kafka/kafka-handler/kafka.producer.ts
+++ saga-pattern/saga-db/Saga_DB_kafka/kafka-handler/kafka.producer.ts
@@ -30,28 +30,24 @@ const runProducer = async (topic: string, data: any) => {
 
     await producer.disconnect();
 };
-const runProducer2 = async (topic: string, data: any) => {
-    console.log("Producer connection started");
-    await producer.connect();
 
 
 
 
 
 
-
-    console.log("Producer connected");
-    console.log("My topic data ", topic, data);
-
-    await producer.send({
-
-        topic: topic,
-        messages: [{ value: JSON.stringify(data) }]
-    });
-    console.log("success topic publish");
-
-    await producer.disconnect();
-};
 
 export default { runProducer }
+
+
+
+
+
+
+
+
+
+
+
+
 // runProducer().catch(console.error);
