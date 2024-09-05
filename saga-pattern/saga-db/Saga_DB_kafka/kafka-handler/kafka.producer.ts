7514384ad12cc6efea19c5afa0f15d691fd60f17
rahulmehata0852 <rahulmahato0852@gmail.com>
2024-09-05 11:36:50 +0530

    new run fn added

--- saga-pattern/saga-db/Saga_DB_kafka/kafka-handler/kafka.producer.ts
+++ saga-pattern/saga-db/Saga_DB_kafka/kafka-handler/kafka.producer.ts
@@ -18,10 +18,28 @@ const runProducer = async (topic: string, data: any) => {
 
 
 
+    console.log("Producer connected");
+    console.log("My topic data ", topic, data);
+
+    await producer.send({
+
+        topic: topic,
+        messages: [{ value: JSON.stringify(data) }]
+    });
+    console.log("success topic publish");
+
+    await producer.disconnect();
+};
+const runProducer2 = async (topic: string, data: any) => {
+    console.log("Producer connection started");
+    await producer.connect();
 
 
 
-    
+
+
+
+
     console.log("Producer connected");
     console.log("My topic data ", topic, data);
 
